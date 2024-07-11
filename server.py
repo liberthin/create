ec87016db12d3f4d812bff0ddb8aae69a290cc69
liberthin <54442345+liberthin@users.noreply.github.com>
2024-07-11 20:53:12 +0000

    Mundo guardado 2024-07-11

--- server.py
+++ server.py
@@ -12,7 +12,7 @@
 # au		      Australia (Sydney)
 # eu		      Europa (Frankfurt)
 # in		      India (Mumbai)
-# jp		      Japón (Tokyo)3
+# jp		      Japón (Tokyo)31
 # sa		      Sudamérica (São Paulo)
 # us		      Estados unidos (Ohio)
 # us-cal-1	      Estados unidos (California)
